37c4c54f167541bcdd0a33da1315c371704c68af
xfl12345 <17960863+xfl12345@users.noreply.github.com>
2022-06-24 14:24:13 +0800

    删除多余代码

--- src/main/java/cc/xfl12345/mybigdata/server/appconst/field/AccountField.java
+++ src/main/java/cc/xfl12345/mybigdata/server/appconst/field/AccountField.java
@@ -1,14 +1,6 @@
 package cc.xfl12345.mybigdata.server.appconst.field;
 
 public class AccountField {
-
-    public static final class ACCOUNT_STATUS {
-        public static final Integer FROZEN = 0;
-        public static final Integer NORMAL = 1;
-        public static final Integer EMAIL_INVALID = 3;
-        public static final Integer EMAIL_NOT_ACTIVATED = 2;
-    }
-
     public static final int USERNAME_MIN_LENGTH = 6;
     public static final int USERNAME_MAX_LENGTH = 32;
     public static final int PASSWORD_MIN_LENGTH = 8;
@@ -18,5 +10,4 @@ public class AccountField {
 
     public static final int PASSWORD_HASH_LENGTH = 128;
     public static final int PASSWORD_SALT_LENGTH = 128;
-
 }
