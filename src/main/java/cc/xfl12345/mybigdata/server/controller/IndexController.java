f91b9a7708f7644a01038178bd20c70a495728e8
xfl12345 <17960863+xfl12345@users.noreply.github.com>
2022-07-02 15:08:20 +0800

    删除JSP代码，准备实行前后端分离。

--- src/main/java/cc/xfl12345/mybigdata/server/controller/IndexController.java
+++ src/main/java/cc/xfl12345/mybigdata/server/controller/IndexController.java
@@ -6,23 +6,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @Controller
 @Slf4j
 public class IndexController {
-    @RequestMapping(path = {"", "index", "index.html"})
-    public String indexView(HttpServletRequest request) {
-        return "index";
-    }
-
-    @RequestMapping("hello_springmvc")
-    public ModelAndView helloSpringmvcView() {
-        ModelAndView modelAndView = new ModelAndView();
-        modelAndView.setViewName("hello_springmvc");
-        modelAndView.addObject("title", "这是个可以在Controller里改的Title");
-        modelAndView.addObject("msg", "Hello SpringMVC!");
-
-
-        return modelAndView;
+    @RequestMapping(path = {"", "index"})
+    public void indexView(HttpServletResponse response) throws IOException {
+        response.sendRedirect("/index.html");
     }
 }
