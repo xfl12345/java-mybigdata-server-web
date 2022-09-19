4c4c200d411ead55f331cad8adc632a8efb65151
xfl12345 <17960863+xfl12345@users.noreply.github.com>
2022-09-19 13:56:43 +0800

    更新依赖 server-mysql-spring-boot-starter 到 Git 版本 29200efe5e ，实现 内容前缀匹配检索 和 数据库概况 Web API

---
+++ src/main/java/cc/xfl12345/mybigdata/server/web/controller/restful/DatabaseViewerController.java
@@ -0,0 +1,42 @@
+package cc.xfl12345.mybigdata.server.web.controller.restful;
+
+import cc.xfl12345.mybigdata.server.common.web.mapper.DatabaseViewer;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@Slf4j
+@RequestMapping("db/viewer")
+public class DatabaseViewerController {
+    protected DatabaseViewer databaseViewer;
+
+    @Autowired
+    public void setDatabaseViewer(DatabaseViewer databaseViewer) {
+        this.databaseViewer = databaseViewer;
+    }
+
+    @GetMapping("all-table-name")
+    public List<String> getAllTableName() {
+        return databaseViewer.getAllTableName();
+    }
+
+    @GetMapping("table/field")
+    public List<String> getTableFieldNames(String tableName) {
+        return databaseViewer.getTableFieldNames(tableName);
+    }
+
+    @GetMapping("table/count")
+    public long getTableRecordCount(String tableName) {
+        return databaseViewer.getTableRecordCount(tableName);
+    }
+
+    @GetMapping("table/content")
+    public List<Object> getTableContent(String tableName, long offset, long limit) {
+        return databaseViewer.getTableContent(tableName, offset, limit);
+    }
+}
---
+++ src/main/java/cc/xfl12345/mybigdata/server/web/controller/restful/SearchController.java
@@ -0,0 +1,45 @@
+package cc.xfl12345.mybigdata.server.web.controller.restful;
+
+import cc.xfl12345.mybigdata.server.common.appconst.api.result.JsonApiResult;
+import cc.xfl12345.mybigdata.server.common.data.condition.SingleTableCondition;
+import cc.xfl12345.mybigdata.server.common.web.mapper.AdvanceSearchMapper;
+import cc.xfl12345.mybigdata.server.web.pojo.WebJsonApiResponseData;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@Slf4j
+@RequestMapping("search")
+public class SearchController {
+    protected AdvanceSearchMapper advanceSearchMapper;
+
+    @Autowired
+    public void setAdvanceSearchMapper(AdvanceSearchMapper advanceSearchMapper) {
+        this.advanceSearchMapper = advanceSearchMapper;
+    }
+
+    @GetMapping("string/by-prefix/{prefix:^\\w+}")
+    public WebJsonApiResponseData searchString(@PathVariable String prefix) {
+        WebJsonApiResponseData responseData = new WebJsonApiResponseData();
+        responseData.setApiResult(JsonApiResult.SUCCEED);
+        responseData.setData(advanceSearchMapper.selectStringByPrefix(prefix));
+        return responseData;
+    }
+
+    @GetMapping("number/by-prefix/{prefix:^\\w+}")
+    public WebJsonApiResponseData searchNumber(@PathVariable String prefix) {
+        WebJsonApiResponseData responseData = new WebJsonApiResponseData();
+        responseData.setApiResult(JsonApiResult.SUCCEED);
+        responseData.setData(advanceSearchMapper.selectNumberByPrefix(prefix));
+        return responseData;
+    }
+
+    @PostMapping("by-condition")
+    public WebJsonApiResponseData searchNumber(SingleTableCondition condition) {
+        WebJsonApiResponseData responseData = new WebJsonApiResponseData();
+        responseData.setApiResult(JsonApiResult.SUCCEED);
+        responseData.setData(advanceSearchMapper.selectByCondition(condition));
+        return responseData;
+    }
+}
