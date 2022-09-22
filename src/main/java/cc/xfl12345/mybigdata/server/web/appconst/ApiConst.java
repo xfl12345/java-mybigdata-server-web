6aa3d7b67142c53b95b16610f0ab8bf2d9bf7be7
xfl12345 <17960863+xfl12345@users.noreply.github.com>
2022-09-22 10:51:28 +0800

    添加新常量——后端 HTTP API 路径名称。

--- src/main/java/cc/xfl12345/mybigdata/server/web/appconst/ApiConst.java
+++ src/main/java/cc/xfl12345/mybigdata/server/web/appconst/ApiConst.java
@@ -2,4 +2,6 @@ package cc.xfl12345.mybigdata.server.web.appconst;
 
 public class ApiConst {
     public static final String VERSION = "1";
+
+    public static final String BACKEND_PATH_NAME = "backend";
 }
