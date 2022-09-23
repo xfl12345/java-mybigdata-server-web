d3f9804c9cef1358f950a47925921572f3086325
xfl12345 <17960863+xfl12345@users.noreply.github.com>
2022-09-23 15:50:15 +0800

    为避免被收藏了错误的网址，访问根路径，不再 forward ，直接 redirect 到正确网址，工作良好。

--- src/main/java/cc/xfl12345/mybigdata/server/web/controller/RedirectController.java
+++ src/main/java/cc/xfl12345/mybigdata/server/web/controller/RedirectController.java
@@ -3,15 +3,13 @@ package cc.xfl12345.mybigdata.server.web.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @Controller
 public class RedirectController {
     @RequestMapping(path = {"", "index"})
-    public void redirectBackend(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.getRequestDispatcher("/index.html").forward(request, response);
+    public void redirectBackend(HttpServletResponse response) throws IOException {
+        response.sendRedirect("./index.html");
     }
 }
