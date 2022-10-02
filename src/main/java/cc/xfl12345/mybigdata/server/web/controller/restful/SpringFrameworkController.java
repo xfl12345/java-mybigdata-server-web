7040ea6efff1646afb53bc28a8e1f4fb7601c300
xfl12345 <17960863+xfl12345@users.noreply.github.com>
2022-10-02 17:57:40 +0800

    减轻 Spring 侵入性，明确 API 请求方法。

--- src/main/java/cc/xfl12345/mybigdata/server/web/controller/restful/SpringFrameworkController.java
+++ src/main/java/cc/xfl12345/mybigdata/server/web/controller/restful/SpringFrameworkController.java
@@ -1,15 +1,12 @@
 package cc.xfl12345.mybigdata.server.web.controller.restful;
 
 import cc.xfl12345.mybigdata.server.web.appconst.ApiConst;
-import lombok.NonNull;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.BeansException;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.AbstractRefreshableApplicationContext;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,66 +18,70 @@ import javax.servlet.http.HttpServletRequest;
 @RestController
 @Slf4j
 @RequestMapping(ApiConst.BACKEND_PATH_BASE + '/' + "spring")
-public class SpringFrameworkController implements ApplicationContextAware {
-
-    protected ConfigurableApplicationContext applicationContext;
-
-    @Autowired
-    @Override
-    public void setApplicationContext(@NonNull ApplicationContext applicationContext) throws BeansException {
-        this.applicationContext = (ConfigurableApplicationContext) applicationContext;
-    }
-
-    @RequestMapping("reload/all")
+public class SpringFrameworkController {
+    @GetMapping("reload/all")
     @ResponseBody
-    public String reloadAll(HttpServletRequest request) {
+    public String reloadAll(HttpServletRequest request, boolean confirm) {
+        if (!confirm) {
+            return "cancelled";
+        }
+
         WebApplicationContext springMVCContext = RequestContextUtils.findWebApplicationContext(request);
         ApplicationContext parent = null;
 
-        if (springMVCContext == null) {
-            parent = applicationContext.getParent();
-        } else {
+        if (springMVCContext != null) {
             parent = springMVCContext.getParent();
-        }
 
-        if (parent != null) {
-            log.info("Root webApplicationContext was found.Refreshing...");
-            ((AbstractRefreshableApplicationContext) parent).refresh();
-        } else {
-            log.info("Root webApplicationContext was not found.");
-            applicationContext.refresh();
-        }
-
-        return "success";
-    }
-
-    @RequestMapping("reload")
-    @ResponseBody
-    public String reloadSpringContext() {
-        applicationContext.refresh();
-
-        return "success";
-    }
-
-    @RequestMapping("shutdown")
-    @ResponseBody
-    public boolean shutdown(HttpServletRequest request, Boolean confirm) {
-        if (confirm == null) {
-            // TODO 写个页面
-        } else {
-            if (confirm) {
-                // 三秒后执行退出任务
-                Thread thread = new Thread(() -> {
-                    try {
-                        Thread.sleep(3000);
-                    } catch (InterruptedException e) {
-                        log.error(e.getMessage());
-                    }
-                    SpringApplication.exit(applicationContext);
-                });
-                thread.start();
-                return true;
+            if (parent != null) {
+                log.info("Root webApplicationContext was found.Refreshing...");
+                ((AbstractRefreshableApplicationContext) parent).refresh();
+            } else {
+                log.info("Root webApplicationContext was not found.");
+                ((ConfigurableApplicationContext) springMVCContext).refresh();
             }
+
+            return "succeed";
+        }
+
+        return "failed";
+    }
+
+    @GetMapping("reload")
+    @ResponseBody
+    public String reloadSpringContext(HttpServletRequest request, boolean confirm) {
+        if (!confirm) {
+            return "cancelled";
+        }
+
+        WebApplicationContext springMVCContext = RequestContextUtils.findWebApplicationContext(request);
+        if (springMVCContext == null) {
+            return "failed";
+        } else {
+            ((ConfigurableApplicationContext) springMVCContext).refresh();
+            return "succeed";
+        }
+    }
+
+    @GetMapping("shutdown")
+    @ResponseBody
+    public boolean shutdown(HttpServletRequest request, boolean confirm) {
+        if (confirm) {
+            WebApplicationContext springMVCContext = RequestContextUtils.findWebApplicationContext(request);
+            if (springMVCContext == null) {
+                return false;
+            }
+
+            // 三秒后执行退出任务
+            Thread thread = new Thread(() -> {
+                try {
+                    Thread.sleep(3000);
+                } catch (InterruptedException e) {
+                    log.error(e.getMessage());
+                }
+                SpringApplication.exit(springMVCContext);
+            });
+            thread.start();
+            return true;
         }
 
         return false;
