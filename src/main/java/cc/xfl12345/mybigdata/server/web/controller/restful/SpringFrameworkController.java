95ed0f13ef230704d5250a74baa415960ea39ed6
xfl12345 <17960863+xfl12345@users.noreply.github.com>
2022-10-02 17:58:49 +0800

    删除无用的、不可正常使用的代码。

--- src/main/java/cc/xfl12345/mybigdata/server/web/controller/restful/SpringFrameworkController.java
+++ src/main/java/cc/xfl12345/mybigdata/server/web/controller/restful/SpringFrameworkController.java
@@ -3,9 +3,6 @@ package cc.xfl12345.mybigdata.server.web.controller.restful;
 import cc.xfl12345.mybigdata.server.web.appconst.ApiConst;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.support.AbstractRefreshableApplicationContext;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -19,49 +16,6 @@ import javax.servlet.http.HttpServletRequest;
 @Slf4j
 @RequestMapping(ApiConst.BACKEND_PATH_BASE + '/' + "spring")
 public class SpringFrameworkController {
-    @GetMapping("reload/all")
-    @ResponseBody
-    public String reloadAll(HttpServletRequest request, boolean confirm) {
-        if (!confirm) {
-            return "cancelled";
-        }
-
-        WebApplicationContext springMVCContext = RequestContextUtils.findWebApplicationContext(request);
-        ApplicationContext parent = null;
-
-        if (springMVCContext != null) {
-            parent = springMVCContext.getParent();
-
-            if (parent != null) {
-                log.info("Root webApplicationContext was found.Refreshing...");
-                ((AbstractRefreshableApplicationContext) parent).refresh();
-            } else {
-                log.info("Root webApplicationContext was not found.");
-                ((ConfigurableApplicationContext) springMVCContext).refresh();
-            }
-
-            return "succeed";
-        }
-
-        return "failed";
-    }
-
-    @GetMapping("reload")
-    @ResponseBody
-    public String reloadSpringContext(HttpServletRequest request, boolean confirm) {
-        if (!confirm) {
-            return "cancelled";
-        }
-
-        WebApplicationContext springMVCContext = RequestContextUtils.findWebApplicationContext(request);
-        if (springMVCContext == null) {
-            return "failed";
-        } else {
-            ((ConfigurableApplicationContext) springMVCContext).refresh();
-            return "succeed";
-        }
-    }
-
     @GetMapping("shutdown")
     @ResponseBody
     public boolean shutdown(HttpServletRequest request, boolean confirm) {
