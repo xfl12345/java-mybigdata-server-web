18792edfe7c5fb64a1b69b2f24c99e8fbb3f6bf0
xfl12345 <17960863+xfl12345@users.noreply.github.com>
2022-10-04 14:20:38 +0800

    再次整顿 logback 配置。

--- src/main/java/cc/xfl12345/mybigdata/server/web/initializer/EnvironmentOnCreatedInitializer.java
+++ src/main/java/cc/xfl12345/mybigdata/server/web/initializer/EnvironmentOnCreatedInitializer.java
@@ -1,5 +1,6 @@
 package cc.xfl12345.mybigdata.server.web.initializer;
 
+import cc.xfl12345.mybigdata.server.common.appconst.AppConst;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.env.EnvironmentPostProcessor;
 import org.springframework.core.Ordered;
@@ -15,6 +16,12 @@ import java.util.Properties;
 public class EnvironmentOnCreatedInitializer implements EnvironmentPostProcessor, Ordered {
     public static final String APP_LOGGING_CONSOLE_LEVEL = "app.logging.console.level";
 
+    public static final String APP_LOGGING_FILE_PATH = "app.logging.file.path";
+
+    public static final String SPRING_APPLICATION_NAME = "spring.application.name";
+
+    public static final String LOGGING_FILE_NAME = "logging.file.name";
+
     @SuppressWarnings("unchecked")
     @Override
     public void postProcessEnvironment(ConfigurableEnvironment environment, SpringApplication application) {
@@ -29,22 +36,35 @@ public class EnvironmentOnCreatedInitializer implements EnvironmentPostProcessor
             }
         }
 
-        String appName = environment.getProperty("spring.application.name");
-
-        String classLoaderName = Thread.currentThread().getContextClassLoader().getClass().getCanonicalName();
-        String logBaseFolder = null;
-        String home2LogPath = "/logs/" + appName + '/';
-        // 判断是否在 Tomcat 容器下运行，决定 log 日志文件保存目录
-        // org.apache.catalina.loader.ParallelWebappClassLoader
-        if (classLoaderName.startsWith("org.apache.catalina") && System.getProperty("catalina.home") != null) {
-            logBaseFolder = (new File(System.getProperty("catalina.home") + home2LogPath)).toString();
-        } else {
-            logBaseFolder = (new File(System.getProperty("user.home") + home2LogPath)).toString();
+        String appName = environment.getProperty(SPRING_APPLICATION_NAME);
+        if (appName == null || "".equals(appName)) {
+            appName = AppConst.DEFAULT_APP_NAME;
+            properties.setProperty(SPRING_APPLICATION_NAME, appName);
         }
-        properties.setProperty("app.logging.file.path", logBaseFolder);
+
+        String fileNameBase = environment.getProperty(LOGGING_FILE_NAME);
+        if (fileNameBase == null || "".equals(fileNameBase)) {
+            fileNameBase = appName;
+            properties.setProperty(LOGGING_FILE_NAME, fileNameBase);
+        }
+
+        String logBaseFolder = environment.getProperty("logging.file.path");
+        if (logBaseFolder == null || "".equals(logBaseFolder)) {
+            String home2LogPath = "/logs/" + appName + '/';
+            // 判断是否在 Tomcat 容器下运行，决定 log 日志文件保存目录
+            // org.apache.catalina.loader.ParallelWebappClassLoader
+            String classLoaderName = Thread.currentThread().getContextClassLoader().getClass().getCanonicalName();
+            if (classLoaderName.startsWith("org.apache.catalina") && System.getProperty("catalina.home") != null) {
+                logBaseFolder = (new File(System.getProperty("catalina.home") + home2LogPath)).toString();
+            } else {
+                logBaseFolder = (new File(System.getProperty("user.home") + home2LogPath)).toString();
+            }
+        }
+
+        properties.setProperty(APP_LOGGING_FILE_PATH, logBaseFolder);
 
         MutablePropertySources propertySources = environment.getPropertySources();
-        PropertiesPropertySource propertiesPropertySource = new PropertiesPropertySource("myDynamicLogConfig", properties);
+        PropertiesPropertySource propertiesPropertySource = new PropertiesPropertySource("myDiyEnv", properties);
         propertySources.addFirst(propertiesPropertySource);
     }
 
