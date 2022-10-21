f9ef2ee9046eb28e87ad39ec97be2da625b4d86e
xfl12345 <17960863+xfl12345@users.noreply.github.com>
2022-10-21 15:40:29 +0800

    通过 日志系统 输出 最终控制台字符编码（同时记录到文件里）。

--- src/main/java/cc/xfl12345/mybigdata/server/web/initializer/MyBeanFactoryPostProcessor.java
+++ src/main/java/cc/xfl12345/mybigdata/server/web/initializer/MyBeanFactoryPostProcessor.java
@@ -6,14 +6,17 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.core.env.ConfigurableEnvironment;
 import org.springframework.stereotype.Component;
 
 @Component
 @Slf4j
 public class MyBeanFactoryPostProcessor implements BeanFactoryPostProcessor {
-
     @Override
     public void postProcessBeanFactory(@NonNull ConfigurableListableBeanFactory configurableListableBeanFactory) throws BeansException {
+        ConfigurableEnvironment configurableEnvironment = configurableListableBeanFactory.getBean(ConfigurableEnvironment.class);
+        log.info("Final console charset name is [" + configurableEnvironment.getProperty("logging.charset.console") + "]");
         // 优先初始化一些 Bean
+        // Something else
     }
 }
