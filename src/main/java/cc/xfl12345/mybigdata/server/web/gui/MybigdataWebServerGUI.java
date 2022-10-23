467a3403179fd0b1ef1d96c27bbab63c9aa65145
xfl12345 <17960863+xfl12345@users.noreply.github.com>
2022-10-23 15:12:28 +0800

    删除废弃代码。

--- src/main/java/cc/xfl12345/mybigdata/server/web/gui/MybigdataWebServerGUI.java
+++ src/main/java/cc/xfl12345/mybigdata/server/web/gui/MybigdataWebServerGUI.java
@@ -3,10 +3,6 @@ package cc.xfl12345.mybigdata.server.web.gui;
 import cc.xfl12345.mybigdata.server.web.MybigdataApplication;
 import cc.xfl12345.mybigdata.server.web.appconst.EnvConst;
 import jp.uphy.javafx.console.ConsoleApplication;
-// import net.sourceforge.argparse4j.ArgumentParsers;
-// import net.sourceforge.argparse4j.inf.ArgumentParser;
-// import net.sourceforge.argparse4j.inf.ArgumentParserException;
-// import net.sourceforge.argparse4j.inf.Namespace;
 
 public class MybigdataWebServerGUI extends ConsoleApplication {
     @Override
@@ -22,18 +18,6 @@ public class MybigdataWebServerGUI extends ConsoleApplication {
     }
 
     public static void main(String[] args) throws Exception {
-        // ArgumentParser parser = ArgumentParsers.newFor("MyBigData server").build().defaultHelp(true);
-        // parser.addArgument("--app.gui.enabled")
-        //     .dest("app.gui.enabled")
-        //     .type(Boolean.class)
-        //     .choices(Boolean.TRUE, Boolean.FALSE)
-        //     .setDefault(Boolean.FALSE)
-        //     .required(false)
-        //     .help("Enable GUI or not. Default is 'false'.");
-        // Namespace ns = null;
-        // ns = parser.parseKnownArgs(args, null);
-        // Boolean appGuiEnable = ns.getBoolean("app.gui.enabled");
-
         String appGuiEnable = System.getProperty(EnvConst.APP_GUI_ENABLED);
         if (appGuiEnable != null && !"".equals(appGuiEnable) && Boolean.parseBoolean(appGuiEnable)) {
             launch(args);
